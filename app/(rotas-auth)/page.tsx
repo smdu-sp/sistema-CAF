593afdc482e075c07be1e9a3b9d28504e939e81a
fernandoMarques <fmarkes48@gmail.com>
2026-04-14 11:36:35 -0300

    upping schemas

--- app/(rotas-auth)/page.tsx
+++ app/(rotas-auth)/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { auth } from "@/lib/auth/auth";
 
 export default async function HomeAuth() {
+  console.log(require('crypto').randomBytes(32).toString('base64'))
   const session = await auth();
   if (!session) redirect("/login");
   const permissao = (session as any).usuario?.permissao;

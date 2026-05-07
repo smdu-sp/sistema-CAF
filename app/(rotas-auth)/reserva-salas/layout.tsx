42e426bd0f70940488929a3702576004c8ea2282
fernandoMarques <fmarkes48@gmail.com>
2026-05-07 14:52:40 -0300

    fixing layout

--- app/(rotas-auth)/reserva-salas/layout.tsx
+++ app/(rotas-auth)/reserva-salas/layout.tsx
@@ -1,10 +1,12 @@
+"use server";
+
 import { TabsNav } from "@/components/tabs-nav";
 import { abasReservaSalas } from "./abas";
 import Titulo from "@/components/titulo";
 import { auth } from "@/lib/auth/auth";
 import { redirect } from "next/navigation";
 import { filtrarAbasPorPermissao } from "./_components/filtrar-abas";
-
+ 
 export default async function LayoutReservaSalas({
   children,
 }: {
@@ -13,12 +15,12 @@ export default async function LayoutReservaSalas({
   const session = await auth();
   if (!session) redirect("/login");
   const usuario = (session as any).usuario;
-
+ 
   const abasPermitidas = filtrarAbasPorPermissao(usuario, abasReservaSalas);
-
+ 
   return (
     <div className="w-full h-full flex flex-col">
-      <TabsNav abas={abasPermitidas} />
+      <TabsNav abas={abasPermitidas} session={session} />
       <Titulo abas={abasPermitidas} />
       {children}
     </div>
